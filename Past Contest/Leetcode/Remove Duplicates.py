ed5e1861813f98508e6c50e1ccf0695dc11bc541
Anthony Perez <stockcart21@yahoo.com>
2018-05-19 22:30:11 -0700

    Create Remove Duplicates.py

---
+++ Past Contest/Leetcode/Remove Duplicates.py
@@ -0,0 +1,17 @@
+class ListNode(object):
+   def __init__(self, x):
+       self.val = x
+       self.next = None
+       
+class Solution(object):
+   def deleteDuplicates(self, head):
+   """ type head: ListNode, rtype: ListNode
+   node = head
+   
+   while node and head node.next:
+      if node.val == node.next.val:
+         node.next = node.next.next
+      else:
+         node = node.next
+   
+   return head
