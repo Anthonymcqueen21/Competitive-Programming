287f5d99a6c7a945f2297b9611f4bc5009865e7f
Anthony Perez <stockcart21@yahoo.com>
2018-11-24 16:41:23 -0800

    Add files via upload

---
+++ Past Contest/Code Forces/Div 2 Contest/DoubleLinkedList.py
@@ -0,0 +1,47 @@
+# Learn More Python the hard way.
+# Double Linked List
+class DoubleLinkedList(object):
+  def __init__(self,value,nxt,prev):
+    self.value = value
+    self.next = next
+    self.prev = prev
+    
+class DoubleLinkedList(object):
+  def __init__(self,first,middle,last):
+    self.first = first
+    self.next = middle
+    self.last = last
+    
+class DoubleLinkedList(object):
+  def __init__(self,Aidan,Israel,Perez):
+    self.Aidan = Aidan
+    self.Israel = Israel
+    self.Perez = Perez
+    
+class DoubleLinkedList(object):
+  def __init__(self,Temper,Tantrum,Mean):
+    self.Temper = Temper
+    self.Tantrum = Tantrum
+    self.Mean = Mean
+    
+class DoubleLinkedList(object):
+  def __init__(self,inshape,crossfit,homegym):
+    self.inshape = inshape
+    self.crossfit = crossfit
+    self.homegym = homegym
+    
+class DoubleLinkedList(object):
+  def __init__(self,planet,earth,fragile):
+    self.planet = planet
+    self.earth = earth
+    self.fragile = fragile
+
+class DoubleLinkedList(object):
+  def __init__(self,crying,temper,mad):
+    self.crying = crying
+    self.temper = temper
+    self.mad = mad
+    
+
+    
+  
