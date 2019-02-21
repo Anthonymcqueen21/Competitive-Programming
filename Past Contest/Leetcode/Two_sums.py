bfd4810c0555f7aa296b92289bc84b9c3b731704
Anthony Perez <stockcart21@yahoo.com>
2019-02-20 21:07:55 -0800

    Update Two_sums.py

--- Past Contest/Leetcode/Two_sums.py
+++ Past Contest/Leetcode/Two_sums.py
@@ -4,7 +4,6 @@ class Solution(object):
          for i, num in enumerate(nums):
              if target - num in num_to_index:
                 return [i, num_to_index[target - num]]
-                
              num_to_index[num] = i
              
          return [] # no sums
