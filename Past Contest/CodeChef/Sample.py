337995fae4c011cc9904c7795c381451ffd2d308
Anthony Perez <stockcart21@yahoo.com>
2019-03-01 20:07:08 -0800

    Update Sample.py

--- Past Contest/CodeChef/Sample.py
+++ Past Contest/CodeChef/Sample.py
@@ -1,5 +1,6 @@
 def function(self, name):
-     self.name = (name, size):
-     self.place = (name, place):
-     self.area = (name, area):
+     self.name = (name, size)
+     self.place = (name, place)
+     self.area = (name, area)
+     
 return name
