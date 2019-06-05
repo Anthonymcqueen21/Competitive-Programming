9af3ec99ff059ef9e18e6eee413d02a9b5584587
Anthony Perez <stockcart21@yahoo.com>
2019-06-05 07:38:05 -0700

    Create Palindrome_Number.py

---
+++ Leet Code/Training_Practice/Palindrome_Number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+     def isPalindrome(self, x):
+     
+     s = str(x)
+     left, right = 0, len(s)-1
+     
+     while left < right:
+       if s[left] != s[right]:
+           return False
+       left += 1
+       right -= 1
+       
+     return True
