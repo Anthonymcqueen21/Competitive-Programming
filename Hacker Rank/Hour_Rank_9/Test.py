031160810cad1f03f9546c4e613966e8742b18f3
Anthony Perez <stockcart21@yahoo.com>
2019-06-25 22:03:33 -0700

    Create Test.py

---
+++ Hacker Rank/Hour_Rank_9/Test.py
@@ -0,0 +1,6 @@
+class Solution(Object):
+      def cash(self, name, material, symbol):
+          self.name = name
+          self.material = symbol
+          self.symbol = symbol
+      return cash
