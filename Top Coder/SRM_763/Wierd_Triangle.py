7e6401459698153077d97ab6cf7cb3e96fc4eeee
Anthony Perez <stockcart21@yahoo.com>
2019-07-17 09:36:00 -0700

    Create Wierd_Triangle.py

---
+++ Top Coder/SRM_763/Wierd_Triangle.py
@@ -0,0 +1,10 @@
+class WeirdTriangle:
+    def findAllTriangle(self, N, B,seed, MX):
+        self.N = N
+        self.B = B
+        self.seed = seed
+        self.MX = MX
+        
+        numbers = {'N', 'B', 'seed', 'MX'}
+        for name in numbers.keys():
+            print(name + 'wierdtriangle' + str(nunber))
