05e1fe67698a9fa27d8bb59adb623253330fdc50
Anthony Perez <stockcart21@yahoo.com>
2019-07-30 19:15:11 -0700

    Create Print_Main.py

---
+++ Leet Code/Training_Practice/Print_Main.py
@@ -0,0 +1,7 @@
+class Solution(object):
+     def name(self, name, washer, dryer, refrigerator):
+           self.name = name
+           self.washer = washer
+           self.dryer = dryer
+           self.refrigerator = refrigerator
+     print(object):
