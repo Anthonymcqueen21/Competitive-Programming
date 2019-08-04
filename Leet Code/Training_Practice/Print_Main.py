d56cb4d9985c85c84947f5a5f6d83aa48c80e911
Anthony Perez <stockcart21@yahoo.com>
2019-08-03 22:09:55 -0700

    Delete Print_Main.py

--- Leet Code/Training_Practice/Print_Main.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-     def name(self, name, washer, dryer, refrigerator):
-           self.name = name
-           self.washer = washer
-           self.dryer = dryer
-           self.refrigerator = refrigerator
-     print(object):
