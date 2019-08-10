5f27761b574678e6492dbba59e158c2d87d534be
Anthony Perez <stockcart21@yahoo.com>
2019-08-09 21:13:29 -0700

    Delete Distribute_Candies.py

--- Past Contest/Leetcode/Distribute_Candies.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-   def distributionCandies(self, candies):
-       """
-       :type candies List[int]
-       :rtype: int
-       """
-       return min(len(candies) // 2, len(set(candies)))
-       
