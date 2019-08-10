5f2025f8c128975420bcbef095c778dc229e8f1d
Anthony Perez <stockcart21@yahoo.com>
2019-08-09 21:13:05 -0700

    Delete Add Digits.py

--- Past Contest/Leetcode/Add Digits.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-   def addDigits(self, num):
-      """
-      type num:int
-      rtype: int
-      """
-      while num > 9:
-          num = sum([int(c) for in str(num)])
-return num
