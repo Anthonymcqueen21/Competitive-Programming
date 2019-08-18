1abaaf563c8ef5c18bedb623dfc17685eb20be65
Anthony Perez <stockcart21@yahoo.com>
2019-08-17 22:21:22 -0700

    Delete Inital Sum.py

--- Past Contest/SoloLearn/Inital Sum.py
+++
@@ -1,8 +0,0 @@
-def mean(*args):
-    """Returns the mean of all the numbers"""
-    total_sum = 0
-    n = len(args)
-    for x in 3,4:
-      total_sum = total_sum + x
-    return total_sum/n
-print((mean(3,4), mean(40,45,50)))
