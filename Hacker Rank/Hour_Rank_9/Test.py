b069fab2a1555206d969c44152dd502aeb1a561f
Anthony Perez <stockcart21@yahoo.com>
2019-11-05 09:28:21 -0800

    Delete Test.py

--- Hacker Rank/Hour_Rank_9/Test.py
+++
@@ -1,6 +0,0 @@
-class Solution(Object):
-      def cash(self, name, material, symbol):
-          self.name = name
-          self.material = symbol
-          self.symbol = symbol
-      return cash
