537cb2f7310a7654e1782f7770dd7c7987c5f1af
Anthony Perez <stockcart21@yahoo.com>
2019-11-16 09:50:41 -0800

    Create Single Push.py

---
+++ CodeForces/Round 600 Div 2/Single Push.py
@@ -0,0 +1,33 @@
+class solution(object):
+    def single_Push(self, operation):
+        self.operation = operation
+        for self.operation in range(operation):
+            print(object)
+        # Print if the names match up
+        # Control flows
+    a = {4, 6, 3, 7, 1, 4, 1, 23, 7, 3, 6, 3, 2, 5, 1, 1, 1, 1, 1, 1, 2, 1, 3, 1, 2, 42, 42, 42, 42, 1, 7, 6}
+    b = {4, 6, 3, 7, 1, 4, 1, 23, 7, 3, 6, 3, 2, 5, 1, 1, 1, 1, 1, 1, 2, 1, 3, 1, 2, 42, 42, 42, 42, 1, 7, 6}
+    # Setting the arrays in place
+    a = 20
+    if a == 20:
+        print("YES")
+    else:
+        print("NO")
+        #Determinating what the control flow is
+    b = 20
+    if b == 100:
+        print("YES")
+    else:
+        print("NO")
+        # Control Flow
+    a = 50
+    if a == 50:
+        print('YES')
+    else:
+        print('NO')
+        # Control Flow
+    b = 100
+    if b == 50:
+        print('YES')
+    else:
+        print('NO')
