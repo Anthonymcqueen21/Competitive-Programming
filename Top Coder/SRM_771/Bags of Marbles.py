4347ac954c34416e3b4d106e7ae1e1405fb881aa
Anthony Perez <stockcart21@yahoo.com>
2019-11-28 16:49:45 -0800

    Create Bags of Marbles.py

---
+++ Top Coder/SRM_771/Bags of Marbles.py
@@ -0,0 +1,18 @@
+class BagsOfMarbles(object):
+    def removeFewest(self,desired,bagSize,noWhiteBags,noBlackBags,someWhiteBags,someBlackBags):
+        "Set the names of each variable"
+        "Returns a integer"
+    def Redbags(self):
+        self.redbags = bagsize
+        return self.redbags_bagsize
+    def GreenBags(self):
+        self.greenbags = noWhiteBags
+        return self.greenbags_noWhiteBags
+    def Bluebags(self):
+        self.bluebags = someWhiteBags
+        return self.bluebags_someWhiteBags
+    def Fuchsiabags(self):
+        self.fuchsiabags = someBlackBags
+        return self.funchsiabags_someBlackBags
+BagsOfMarbles_obj = BagsOfMarbles()
+print("There are bags:", BagsOfMarbles.getremoveFewest())
