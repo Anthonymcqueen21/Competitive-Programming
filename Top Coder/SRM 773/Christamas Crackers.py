93cc9197f8daae439ea1716132eeee7b76efbba0
Anthony Perez <stockcart21@yahoo.com>
2019-12-24 19:55:08 -0800

    Create Christamas Crackers.py

---
+++ Top Coder/SRM 773/Christamas Crackers.py
@@ -0,0 +1,15 @@
+class ChristmasCrackers:
+    def crack(self,N,K):
+        self.N = N
+        self.K = K
+        if N == 2:
+            print(numbered)
+        elif 2 < 100:
+            print(crack)
+    def numbered(self):
+        self.N = N
+        if N == -1:
+            print(crack)
+        elif 2 < 100:
+            print(numbered)
+    N = people(-1)
