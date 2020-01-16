8b384dfa5b7d347d45616b3c551945e97600c1d0
Anthony Perez <stockcart21@yahoo.com>
2020-01-16 09:14:13 -0800

    Create Completely Different Strings.py

---
+++ Top Coder/SRM 775/Completely Different Strings.py
@@ -0,0 +1,13 @@
+class CompletelyDifferentStrings:
+    """Finding the complete different strings"""
+    def __init__(self,S):
+        self.S = S
+    def count(self,forbidden):
+        print(self.integer)
+        for L in forbidden:
+            print('S')
+        else:
+            print('L')
+        s = [0,1,2]
+        print(sorted_elements(s,desc = False))
+forbidden_obj = S('total numbers')
