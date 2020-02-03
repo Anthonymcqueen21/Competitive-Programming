6ae53532ae83bc3fa87119f966cf6853a2aa780d
Anthony Perez <stockcart21@yahoo.com>
2020-02-03 08:57:35 -0800

    Create Limping God.py

---
+++ Top Coder/SRM 777/Limping God.py
@@ -0,0 +1,8 @@
+class LimpingDog:
+     def countSteps(self, time):
+         self.time = time
+         if '47' == '42':
+             print('two seconds')
+         else:
+             print('one second')
+         return('steps')
