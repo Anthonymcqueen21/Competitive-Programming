3061ccc21b9bdb3bc7b6a845c439f3a7e8f4dfc4
Anthony Perez <stockcart21@yahoo.com>
2020-02-03 09:06:27 -0800

    Create String transformation.py

---
+++ Top Coder/SRM 777/String transformation.py
@@ -0,0 +1,14 @@
+class StringTransformationEasy:
+    def getResult(self,s,t):
+        self.s = s
+        self.t = t
+        if 'red' == 'r':
+            print('red balls')
+        else:
+            print('target')
+        return('YES')
+        if 'green' == 'g':
+            print('green balls')
+        else:
+            print('target')
+        return('NO')
