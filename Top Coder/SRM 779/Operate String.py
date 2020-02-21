c7729938d82c8c542111d3763508535c084a8c7c
Anthony Perez <stockcart21@yahoo.com>
2020-02-21 09:01:52 -0800

    Create Operate String.py

---
+++ Top Coder/SRM 779/Operate String.py
@@ -0,0 +1,16 @@
+class OperateString:
+   def operate(self,s,moves):
+       self.s = s
+       self.moves = moves
+       if x == x:
+          print('x')
+      else:
+          print('zero')
+      if -x == -x:
+          print('-x')
+      else:
+          print('zero')
+          
+          return operate()
+          
+       
