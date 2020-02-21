99fd46f781039725683850fb949e08380f0f22cc
Anthony Perez <stockcart21@yahoo.com>
2020-02-21 09:15:53 -0800

    Create OneGcd.py

---
+++ Top Coder/SRM 779/OneGcd.py
@@ -0,0 +1,10 @@
+class OneGcd:
+    def solve(self,X,Y):
+        self.X = X
+        self.Y = Y
+        for i in range(35):
+            print('X')
+        else:
+            print('Y')
+            
+            return N
