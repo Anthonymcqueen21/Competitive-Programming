ee1b92ceee43a18ded35a88904a910f60170c75d
Anthony Perez <stockcart21@yahoo.com>
2020-04-11 10:07:42 -0700

    Create Cardboard boxes.py

---
+++ Top Coder/SRM 783/Cardboard boxes.py
@@ -0,0 +1,24 @@
+class CardboardBoxes:
+    """define the length width and height of the top and lower"""
+    def topflap(self, S):
+        """Initializing the object"""
+        CardboardBoxes = {'length': 50, 'width': 21}
+        self.S = S
+        if topflap >= 10.5:
+            print('This is the correct size for the top')
+        elif topflap < 10.5:
+            print('This is not the correct size for the top flap')
+        else:
+            CardboardBoxes = 70
+    def bottomflap(self):
+        """Setting the sizes"""
+        if bottomflap == 10.5:
+            print('This is the correct size for the bottom flap')
+        elif bottomflap < 10.5:
+            print('This is the incorrect size for the bottom flap')
+        return boxes
+        
+boxes_cut = CardboardBoxes('flaps')
+
+print(boxes_cut.flaps + 'the size of the overall flaps')
+boxes_cut.flap()
