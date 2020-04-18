4e798c4a6b482099b51701e44db9147ff1799c1c
Anthony Perez <stockcart21@yahoo.com>
2020-04-18 10:11:54 -0700

    Create ThreeNeighbors.py

---
+++ Top Coder/2020 TCO Algorithm Qualifier/ThreeNeighbors.py
@@ -0,0 +1,23 @@
+class ThreeNeighbors:
+    def construct(self,N):
+        """Return the configuration as a"""
+        if X == 'Alive':
+            print('Alive')
+        else:
+            print('Dead')
+        return a
+    def world(self,N):
+        """Define if either alive or dead"""
+        for i in N.values():
+            print(str(number) + 'Its alive')
+        if X == 'Dead':
+            print('50')
+        else:
+            print('Dead')
+        if '.' == 'Alive':
+            print('50')
+        else:
+            print('Dead')
+
+sum = construct(50, 50)
+print(sum)
