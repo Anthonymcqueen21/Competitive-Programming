04286e6db6aa40f69030552a954f49e7eb90f00b
Anthony Perez <stockcart21@yahoo.com>
2020-04-18 10:21:11 -0700

    Create AveragePrice.py

---
+++ Top Coder/2020 TCO Algorithm Qualifier/AveragePrice.py
@@ -0,0 +1,5 @@
+class AveragePrice(object):
+        def nonDuplicatedAverage(self,prices):
+             prices = {"10","20","10"}
+        for prices, number in average.items():
+             print(mean, 'average' + str(number))
